ad4d7577163e493137d8dfd7f4c5cd2c096a39cb
YoungHwa Shin <sinyh104@e-mail.hs.kr>
2016-05-26 20:33:47 +0900

    #2 set on click button flip

--- app/src/main/java/kr/hs/emirim/sinyh104/imageslideshow/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/sinyh104/imageslideshow/MainActivity.java
@@ -2,12 +2,37 @@ package kr.hs.emirim.sinyh104.imageslideshow;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ViewFlipper;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    ViewFlipper flip;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        flip=(ViewFlipper)findViewById(R.id.view_flip);
+        flip.setFlipInterval(1000);
+
+        Button butStart=(Button)findViewById(R.id.but_start);
+        butStart.setOnClickListener(this);
+        Button butStop=(Button)findViewById(R.id.but_stop);
+        butStop.setOnClickListener(this);
+    }
+
+    /**
+     * Called when a view has been clicked.
+     *
+     * @param v The view that was clicked.
+     */
+    @Override
+    public void onClick(View v) {
+        switch (v.getId())
+        {
+            case R.id.but_start : flip.startFlipping();break;
+            case R.id.but_stop : flip.stopFlipping();break;
+        }
     }
 }
